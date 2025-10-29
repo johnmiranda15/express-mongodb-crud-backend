6f9a06ddd30945d27c10fefb54db1fca98102e3d
John Miranda <mirandajohn634@gmail.com>
2025-10-28 19:16:12 -0500

    agrego respuesta get de servidor ejecutandose correctamente

--- src/config.js
+++ src/config.js
@@ -3,6 +3,5 @@ import { config } from "dotenv";
 config();
 
 export const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/test";
-
-
+export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173"
 export const PORT = process.env.PORT || 4000;
